71f93fdda369de651e6a05116b7dee74a2711e43
Ari Horowitz <ariehh@comcast.net>
2020-02-27 15:47:44 -0800

    WIP on orm.js and imported schema into workbench for table visibility

--- config/orm.js
+++ config/orm.js
@@ -2,4 +2,26 @@
 
 const connection = require("../config/connection.js");
 
+// Do I need the following?
+// connection.connect(function(err) {
+//     if (err) {
+//       console.error('error connecting: ' + err.stack);
+//       return;
+//     };
+//     console.log('connected as id ' + connection.threadId);
+//   });
+
+const orm = {
+  selectWhere: function(tableInput, colToSearch, valOfCol) {
+    const queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
+};
+
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE burgers(
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(200) NOT NULL,
     devoured BOOLEAN,
-    date TIMESTAMP,
+    date TIMESTAMP,         -- which date format to utilize?
     PRIMARY KEY (id)
 );
