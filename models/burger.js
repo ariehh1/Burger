9967e3c9e0590bed4bcb3758b71ac92170a597fe
Ari Horowitz <ariehh@comcast.net>
2020-03-01 14:48:29 -0800

    new delete function in burger.js & new Date() in update function

--- models/burger.js
+++ models/burger.js
@@ -4,25 +4,28 @@ const orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
-    orm.allOrder("burgers", function(res) {
+    orm.allOrder("burgers", "date, id", function(res) {
       cb(res);
     });
   },
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  create: function(vals, cb) {
+    orm.create("burgers", ["burger_name"], vals, function(res) {
       cb(res);
     });
   },
-  update: function(id, cb) {
-    var condition = "id= " + id;
-    orm.update(
-      "burgers",
-      {
-        devoured: true
-      },
-      condition,
-      cb
-    );
+  update: function(objColVals, condition, cb) {
+    objColVals.date = new Date()
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " ");
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
   }
 };
 
